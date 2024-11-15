938b66541b35f7ca2c401a48473479ade1de7206
gsyssfdyst <gsyssfdsyt@gmail.com>
2024-11-15 04:54:26 +0000

    test

---
+++ src/AtidadeSalaDeAula.java
@@ -0,0 +1,16 @@
+public class AtidadeSalaDeAula {
+
+    // 1Imprima todos os números de 150 a 300.
+    
+    public static void main(String[] args) {
+          for (int i = 150; <= 300; i ++){
+            System.out.println(" ");
+          }
+    
+    
+    } 
+    
+    
+        
+    }
+    
